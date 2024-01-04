48221073e14c0452874dd9a3a65adfb0bc0e90c8
Lily <ratshate22@gmail.com>
2024-01-04 19:52:16 +1000

    Fixed adress binding bug - TODO: make program not hang on write and read requests

--- src/main.c
+++ src/main.c
@@ -22,10 +22,10 @@ int main(int argc, char** argv) {
     }
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-p") || strcmp(argv[i], "--port")) {
+        if (!strcmp(argv[i], "-p") || !strcmp(argv[i], "--port")) {
             port = argv[++i];
         }
-        else if (strcmp(argv[i], "-h") || strcmp(argv[i], "--host")) {
+        else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--host")) {
             host = argv[++i];
         }
     }
@@ -33,21 +33,32 @@ int main(int argc, char** argv) {
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_port = htons(atoi(port));
-    if (inet_pton(AF_INET, host, &address.sin_family) != 1) {
-        perror("Invalid socket:");
+    if (inet_pton(AF_INET, host, &address.sin_addr.s_addr) != 1) {
+        perror("Invalid socket");
         return errno;
     }
-    address.sin_family = htonl(address.sin_family);
-    // address.sin_family = htonl(INADDR_LOOPBACK);
+    address.sin_addr.s_addr = htonl(address.sin_family);
 
     int connection = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(connection, (struct sockaddr*)&address, sizeof(address)) == -1) {
-        perror("Failed to connect:");
+    if (connection < 0) {
+        perror("Unable to create socket");
         return errno;
     }
+
+    if (connect(connection, (struct sockaddr*)&address, sizeof(address)) == -1) {
+        perror("Failed to connect");
+        return errno;
+    }
+    printf("test\n");
     char* toggle = "pause\n";
-    if (write(connection, toggle, strlen(toggle)) == -1) {
-        perror("Failed to connect:");
+    if (send(connection, toggle, strlen(toggle) + 1, 0) == -1) {
+        perror("Failed to write");
+        return errno;
+    }
+
+    char* buffer = malloc(51*sizeof(char));
+    if (recv(connection, buffer, 50, 0) == -1) {
+        perror("Failed to read");
         return errno;
     }
     return 0;
